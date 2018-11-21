57ca8e940cf88b1a370b1111d6b4d239a41b5c67
oliveiranoel <noel.oliveira@bbzsogr.ch>
2018-11-21 11:51:54 +0100

    added karte file and printed out the file

--- src/bancomat/Bancomat.java
+++ src/bancomat/Bancomat.java
@@ -1,21 +1,49 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bancomat;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import javax.swing.JComboBox;
 
 /**
  *
- * @author noel.oliveira
+ * @author noel.oliveira, dominik.suter
+ * @version 1.0
  */
 public class Bancomat {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException {
+        Bancomat bancomat = new Bancomat();
+        
+        bancomat.show(true);
+        
+    }
+    
+    public void listDir(JComboBox kartenDrop) throws FileNotFoundException, IOException {
+        File f = new File("P:/3_Lehrjahr/M226/UML/bancomat/dateien/karten");
+        File[] files = f.listFiles();
+        
+        if (files != null) { // Erforderliche Berechtigungen etc. sind vorhanden
+            for (int i = 0; i < files.length; i++) {
+                FileReader fr = new FileReader(files[i].getAbsolutePath());
+                BufferedReader br = new BufferedReader(fr);
+
+                String zeile1 = br.readLine();
+                System.out.println(zeile1);
+                
+            }
+        }
+    }
+    
+    public void show(boolean visible) {
+        if (visible) {
+            Kartenschliz kartenschliz = new Kartenschliz();
+            kartenschliz.setVisible(visible);
+        } else {
+            System.out.println("GUI Visible is false. Please set it to true");
+        }
     }
     
 }
